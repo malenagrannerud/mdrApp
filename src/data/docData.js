e337f309a13c1d5979d61a3120d458796d288b9e
malenagrannerud <126866109+malenagrannerud@users.noreply.github.com>
2026-05-24 20:43:57 +0000

    k

--- src/data/docData.js
+++ src/data/docData.js
@@ -8,16 +8,32 @@ ART_61: {
     id: 'ART_61',
     title: 'Clinical evaluation',
     content: `
-# ARTICLE 61 Clinical evaluation
 
-## Chapter I - General Requirements
-Clinical evaluation
 
-1.   Confirmation of conformity with relevant GSPR set out in Annex I under the normal conditions of the intended use of the device, and the evaluation of the undesirable side-effects and of the acceptability of the benefit-risk- ratio referred to in Sections 1 and 8 of Annex I, 
+# ABBREVIATIONS
+Clinical evaluation plan (CEP)
+Clinical evaluation report (CER)
+Post-market clinical follow-up (PMCF)
+General safety and performance requirements (GSPR)
+Conformity assessment (CA)
+Notified body (NB)
+Clinical investigation (CI)
+Clinical data (CD)
+Well-established technologies (WET)
+
+# CHAPTER I - CLINICAL EVALUATION
+
+## 1.   
+Confirmation of 
+- conformity with GSPR in Annex I under normal conditions of intended use, and the 
+- evaluation of undesirable side-effects and of 
+- the acceptability of the benefit-risk- ratio referred to in Sections 1 and 8 of Annex I, 
 shall be based on 
 - clinical data providing sufficient clinical evidence, 
 - including where applicable relevant data as referred to in Annex III.
-The mfr shall specify and justify the level of clinical evidence necessary to demonstrate conformity with the relevant general safety and performance requirements. That level of clinical evidence shall be appropriate in view of the characteristics of the device and its intended purpose.
+
+The mfr shall specify and justify the level of clinical evidence necessary to demonstrate conformity with relevant GSPR. 
+That level of clinical evidence shall be appropriate in view of the characteristics of the device and its intended purpose.
 
 To that end, mfrs shall 
 - plan, 
@@ -25,89 +41,84 @@ To that end, mfrs shall
 - document 
 a clinical evaluation in accordance with this Article and Part A of Annex XIV.
 
-2.   For all class III devices and for the class IIb devices referred to in point (b) of Article 54(1), the manufacturer may, prior to its clinical evaluation and/or investigation, consult an expert panel as referred to in Article 106, with the aim of reviewing the manufacturer's intended clinical development strategy and proposals for clinical investigation. The manufacturer shall give due consideration to the views expressed by the expert panel. Such consideration shall be documented in the clinical evaluation report referred to in paragraph 12 of this Article.
+## 2.   
+For class III and IIb devices referred to in point (b) of Article 54(1), the mfr may, 
+prior to its clinical evaluation and/or investigation, consult an expert panel as referred to in Article 106, 
+with the aim of reviewing the mfrs intended clinical development strategy and proposals for clinical investigation. 
+The mfr shall give due consideration to the views expressed by the expert panel. 
+Such consideration shall be documented in the CER referred to in paragraph 12 of this Article.
 
-The manufacturer may not invoke any rights to the views expressed by the expert panel with regard to any future conformity assessment procedure.
+The mfr may not invoke any rights to the views expressed by the expert panel with regard to any future conformity assessment procedure.
 
-3.   A clinical evaluation shall follow a defined and methodologically sound procedure based on the following:
+## 3.   
+A clinical evaluation shall follow a defined and methodologically sound procedure based on the following:
 
-(a) a critical evaluation of the relevant scientific literature currently available relating to the safety, performance, design characteristics and intended purpose of the device, where the following conditions are satisfied:
+(a) a critical evaluation of the relevant scientific literature currently available relating to the safety, 
+performance, design characteristics and intended purpose of the device, where the following conditions are satisfied:
+— it is demonstrated that the device subject to clinical evaluation for the intended purpose is equivalent to the device to which the data relate,
+ in accordance with Section 3 of Annex XIV, and
+— the data adequately demonstrate compliance with the relevant general safety and performance requirements;
 
-—
-
-it is demonstrated that the device subject to clinical evaluation for the intended purpose is equivalent to the device to which the data relate, in accordance with Section 3 of Annex XIV, and
-
-—
-
-the data adequately demonstrate compliance with the relevant general safety and performance requirements;
-
-(b) a critical evaluation of the results of all available clinical investigations, taking duly into consideration whether the investigations were performed under Articles 62 to 80, any acts adopted pursuant to Article 81, and Annex XV; and
+(b) a critical evaluation of the results of all available clinical investigations, taking duly into consideration whether the investigations were performed under Articles 62 to 80, 
+any acts adopted pursuant to Article 81, and Annex XV; and
 (c) a consideration of currently available alternative treatment options for that purpose, if any.
 
 
-4. In the case of implantable devices and class III devices, clinical investigations shall be performed, except if:
+## 4. 
+In the case of implantable devices and class III devices, clinical investigations shall be performed, except if:
 
-—
-
-the device has been designed by modifications of a device already marketed by the same manufacturer,
-
-—
-
-the modified device has been demonstrated by the manufacturer to be equivalent to the marketed device, in accordance with Section 3 of Annex XIV and this demonstration has been endorsed by the notified body, and
-
-—
-
-the clinical evaluation of the marketed device is sufficient to demonstrate conformity of the modified device with the relevant safety and performance requirements.
+— the device has been designed by modifications of a device already marketed by the same manufacturer,
+— the modified device has been demonstrated by the manufacturer to be equivalent to the marketed device, in accordance with Section 3 of Annex XIV and this demonstration has been endorsed by the notified body, and
+—the clinical evaluation of the marketed device is sufficient to demonstrate conformity of the modified device with the relevant safety and performance requirements.
 
 In this case, the notified body shall check that the PMCF plan is appropriate and includes post market studies to demonstrate the safety and performance of the device.
-
 In addition, clinical investigations need not be performed in the cases referred to in paragraph 6.
 
-5.   A manufacturer of a device demonstrated to be equivalent to an already marketed device not manufactured by him, may also rely on paragraph 4 in order not to perform a clinical investigation provided that the following conditions are fulfilled in addition to what is required in that paragraph:
+## 5.   
+A manufacturer of a device demonstrated to be equivalent to an already marketed device not manufactured by him, may also rely on paragraph 4 in order not to perform a clinical investigation provided that the following conditions are fulfilled in addition to what is required in that paragraph:
 
-—
+— the two manufacturers have a contract in place that explicitly allows the manufacturer of the second device full access to the technical documentation on an ongoing basis, and
+— the original clinical evaluation has been performed in compliance with the requirements of this Regulation,
 
-the two manufacturers have a contract in place that explicitly allows the manufacturer of the second device full access to the technical documentation on an ongoing basis, and
+and the mfr of the second device provides clear evidence thereof to the notified body.
 
-—
+## 6.   
+The requirement to perform clinical investigations pursuant to paragraph 4 shall not apply to implantable devices and class III devices:
 
-the original clinical evaluation has been performed in compliance with the requirements of this Regulation,
+(a) which have been lawfully placed on the market or put into service in accordance with Directive 90/385/EEC or Directive 93/42/EEC and for which the clinical evaluation:
 
-and the manufacturer of the second device provides clear evidence thereof to the notified body.
+— is based on sufficient clinical data, and
+— is in compliance with the relevant product-specific CS for the clinical evaluation of that kind of device, where such a CS is available; or
 
-6.   The requirement to perform clinical investigations pursuant to paragraph 4 shall not apply to implantable devices and class III devices:
+(b) that are sutures, staples, dental fillings, dental braces, tooth crowns, screws, wedges, plates, wires, pins, clips or connectors for which the clinical evaluation is based on sufficient clinical data and is in compliance with the relevant product-specific CS, where such a CS is available.
 
-(a)
+## 7.   
+Cases in which paragraph 4 is not applied by virtue of paragraph 6 shall be justified in the clinical evaluation report by the manufacturer and in the clinical evaluation assessment report by the notified body.
 
-which have been lawfully placed on the market or put into service in accordance with Directive 90/385/EEC or Directive 93/42/EEC and for which the clinical evaluation:
+## 8.   
+Where justified in view of well-established technologies, similar to those used in the exempted devices listed in point (b) of paragraph 6 of this Article, being used in other devices, or where justified in order to protect the health and safety of patients, users or other persons or other aspects of public health, the Commission is empowered to adopt delegated acts in accordance with Article 115 to amend the list of exempted devices referred to in the second subparagraph of Article 52(4) and in point (b) of paragraph 6 of this Article, by adding other types of implantable or class III devices to that list or removing devices therefrom.
 
-—
+## 9.   
+In the case of the products without an intended medical purpose listed in Annex XVI, the requirement to demonstrate a clinical benefit in accordance with this Chapter and Annexes XIV and XV shall be understood as a requirement to demonstrate the performance of the device. Clinical evaluations of those products shall be based on relevant data concerning safety, including data from post-market surveillance, PMCF, and, where applicable, specific clinical investigation. Clinical investigations shall be performed for those products unless reliance on existing clinical data from an analogous medical device is duly justified.
 
-is based on sufficient clinical data, and
+## 10.   
+Without prejudice to §4, where the demonstration of conformity with GSPR based on clinical data is not deemed appropriate, adequate justification for any such exception shall be given based on the results of the mfrs risk management and on consideration of the specifics of the interaction between the device and the human body, the clinical performance intended and the claims of the mfr. In such a case, the mfr shall duly substantiate in the TD as in Annex II why it considers a demonstration of conformity with GSPR that is based on the results of 
+- non-clinical testing methods alone, including 
+  - performance evaluation, 
+  - bench testing and 
+  - pre-clinical evaluation, 
+to be adequate.
 
-—
-
-is in compliance with the relevant product-specific CS for the clinical evaluation of that kind of device, where such a CS is available; or
-
-(b)
-
-that are sutures, staples, dental fillings, dental braces, tooth crowns, screws, wedges, plates, wires, pins, clips or connectors for which the clinical evaluation is based on sufficient clinical data and is in compliance with the relevant product-specific CS, where such a CS is available.
-
-7.   Cases in which paragraph 4 is not applied by virtue of paragraph 6 shall be justified in the clinical evaluation report by the manufacturer and in the clinical evaluation assessment report by the notified body.
-
-8.   Where justified in view of well-established technologies, similar to those used in the exempted devices listed in point (b) of paragraph 6 of this Article, being used in other devices, or where justified in order to protect the health and safety of patients, users or other persons or other aspects of public health, the Commission is empowered to adopt delegated acts in accordance with Article 115 to amend the list of exempted devices referred to in the second subparagraph of Article 52(4) and in point (b) of paragraph 6 of this Article, by adding other types of implantable or class III devices to that list or removing devices therefrom.
-
-9.   In the case of the products without an intended medical purpose listed in Annex XVI, the requirement to demonstrate a clinical benefit in accordance with this Chapter and Annexes XIV and XV shall be understood as a requirement to demonstrate the performance of the device. Clinical evaluations of those products shall be based on relevant data concerning safety, including data from post-market surveillance, PMCF, and, where applicable, specific clinical investigation. Clinical investigations shall be performed for those products unless reliance on existing clinical data from an analogous medical device is duly justified.
-
-10.   Without prejudice to paragraph 4, where the demonstration of conformity with general safety and performance requirements based on clinical data is not deemed appropriate, adequate justification for any such exception shall be given based on the results of the manufacturer's risk management and on consideration of the specifics of the interaction between the device and the human body, the clinical performance intended and the claims of the manufacturer. In such a case, the manufacturer shall duly substantiate in the technical documentation referred to in Annex II why it considers a demonstration of conformity with general safety and performance requirements that is based on the results of non-clinical testing methods alone, including performance evaluation, bench testing and pre-clinical evaluation, to be adequate.
-
-11.   The clinical evaluation and its documentation shall be updated throughout the life cycle of the device concerned with clinical data obtained from the implementation of the manufacturer's PMCF plan in accordance with Part B of Annex XIV and the post-market surveillance plan referred to in Article 84.
+## 11.   
+The clinical evaluation and its documentation shall be updated throughout the life cycle of the device concerned with clinical data obtained from the implementation of the mfrs PMCF plan in accordance with Part B of Annex XIV and the PMS plan referred to in Article 84.
 
 For class III devices and implantable devices, the PMCF evaluation report and, if indicated, the summary of safety and clinical performance referred to in Article 32 shall be updated at least annually with such data.
 
-12.   The clinical evaluation, its results and the clinical evidence derived from it shall be documented in a clinical evaluation report as referred to in Section 4 of Annex XIV, which, except for custom-made devices, shall be part of the technical documentation referred to in Annex II relating to the device concerned.
+## 12.   
+The clinical evaluation, its results and the clinical evidence derived from it shall be documented in a CER as in Section 4 of Annex XIV, which, except for custom-made devices, shall be part of the TD referred to in Annex II relating to the device concerned.
 
-13.   Where necessary to ensure the uniform application of Annex XIV, the Commission may, having due regard to technical and scientific progress, adopt implementing acts to the extent necessary to resolve issues of divergent interpretation and of practical application. Those implementing acts shall be adopted in accordance with the examination procedure referred to in Article 114(3).
+## 13.   
+Where necessary to ensure the uniform application of Annex XIV, the Commission may, having due regard to technical and scientific progress, adopt implementing acts to the extent necessary to resolve issues of divergent interpretation and of practical application. Those implementing acts shall be adopted in accordance with the examination procedure referred to in Article 114(3).
 
 
     `.trim()
