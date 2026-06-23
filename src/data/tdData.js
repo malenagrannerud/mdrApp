40361bf0247edb9f5239e7ba355e6325930ad45e
malenagrannerud <126866109+malenagrannerud@users.noreply.github.com>
2026-06-23 17:17:43 +0000

    k

--- src/data/tdData.js
+++ src/data/tdData.js
@@ -8,19 +8,42 @@
 
 export const TD_DATA = {
 
-  deviceDescription: {
+
+  DD: {
+    id: 'SOP', 
     title: '📄 Device_description_and_rationale.pdf',
+    version: '1.0', 
+    owner: 'R&D',
     content: `
+
     NAN
+
 `.trim()
   },
 
 
-  manufacturingProcess: {
+  MF_P: {
+    id: 'SOP',
     title: '📄 Manufacturing_process_description.pdf',
+    version: '1.0',
+    owner: 'R&D',
     content: `
     NAN
 
 `.trim()
-  }
+  }, 
+
+  RM: {
+    id: 'SOP',
+    title: '📄 Risk_management_procedure.pdf',
+    version: '1.0',
+    owner: 'R&D',
+    content: `
+    NAN
+
+`.trim()
+  },
+
+
+
 };
